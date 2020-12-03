2f83dd0b457af0a905005170cbb3838b5e888573
Isaiah Lemmon <lemmoi@cs.washington.edu>
2020-12-02 16:17:34 -0800

    Sphinx working with numpydoc and type hints

---
+++ docs/source/transition_sampling.engines.rst
@@ -0,0 +1,21 @@
+transition\_sampling.engines package
+====================================
+
+Submodules
+----------
+
+transition\_sampling.engines.abstract\_engine module
+----------------------------------------------------
+
+.. automodule:: transition_sampling.engines.abstract_engine
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: transition_sampling.engines
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- transition_sampling/engines/abstract_engine.py
+++ transition_sampling/engines/abstract_engine.py
@@ -4,7 +4,7 @@ order to be used by the aimless shooting algorithm
 """
 
 from abc import ABC, abstractmethod
-from typing import Sequence
+from typing import Sequence, Tuple
 import numpy as np
 
 
@@ -30,48 +30,72 @@ class AbstractEngine(ABC):
 
     @abstractmethod
     def atoms(self) -> Sequence[str]:
-        """
+        """Get the atoms held by the engine.
+
         Get a sequence of the string representations of the atoms in use by
-        this engine. String representation should be the same as the periodic
-        table.
+        this engine. String representations are the same as the periodic table.
+
+        Returns
+        -------
+        An ordered sequence of atoms in the engine.
         """
         pass
 
     @abstractmethod
     def set_positions(self, positions: np.ndarray) -> None:
-        """
-        Set the positions of atoms in the engine. Positions are ordered for n
-        atoms, in shape (n, 3). Rows represent atoms and columns represent
-        (x, y, z)
+        """Set the positions of atoms in the engine.
 
-        :param positions: The positions for atoms to be set to.
-        :type positions: np.array with shape (n, 3)
+        Positions are ordered for n atoms, in shape (n, 3). Rows represent atoms
+        and columns represent (x, y, z) dimensions.
+
+        Parameters
+        ----------
+        positions : np.ndarray with shape (n, 3)
+            The positions for atoms to be set to.
+
+        Returns
+        -------
+        None
         """
         pass
 
     @abstractmethod
     def set_velocities(self, velocities: np.ndarray) -> None:
-        """
-        Set the velocities of atoms in the engine. Velocities are ordered for n
-        atoms, in shape (n, 3). Rows represent atoms and columns represent
-        (x, y, z)
+        """Set the velocities of atoms in the engine.
 
-        :param velocities: The positions for atoms to be set to.
-        :type velocities: np.array with shape (n, 3)
+        Velocities are ordered for n atoms, in shape (n, 3). Rows represent
+        atoms and columns represent (x, y, z) dimensions.
+
+        Parameters
+        ----------
+        velocities : np.ndarray with shape (n, 3)
+            The positions for atoms to be set to.
+
+        Returns
+        -------
+        None
         """
         pass
 
     @abstractmethod
-    def validate_inputs(self, inputs: dict) -> (bool, str):
-        """
+    def validate_inputs(self, inputs: dict) -> Tuple[bool, str]:
+        """Validate the given inputs for the specific engine.
+
         Given a dictionary input, validate that it represents a well-formed
         input with all requirements for this engine
 
-        :param inputs: dict of engine-specific inputs
-        :type inputs: dict where the engine defines required fields
-
         :return: True and an empty string if input is valid. False and the error
          message otherwise
+
+        Parameters
+        ----------
+        inputs
+            dict of engine-specific inputs
+
+        Returns
+        -------
+        (True, "") if the inputs passed validation. Otherwise,
+        (False, "<Relevant Error Message>")
         """
         if "engine" not in inputs:
             return False, "engine must be specified in inputs"
@@ -83,46 +107,53 @@ class AbstractEngine(ABC):
 
     @abstractmethod
     async def run_shooting_point(self) -> ShootingResult:
-        """
+        """Run the forward and reverse trajectories to get a shooting point.
+
         Launch the MD simulation in both the forward and reverse direction
         from the assigned starting points and velocities, in parallel. These
         are spawned in new processes. Awaiting this waits for both
         simulations to commit to a basin or time out
 
-        :return: The positions of the +/- dt frames, as well as the committing
-            results of both simulations.
-        :rtype: ShootingResult
+        Returns
+        -------
+        The positions of the +/- dt frames, as well as the committing
+        results of both simulations.
         """
         pass
 
     @property
     @abstractmethod
     def delta_t(self) -> float:
-        """
-        Get the time offset this engine is set to capture in seconds
+        """Get the time offset this engine is set to capture in seconds
 
-        :return: The time offset of this engine
-        :rtype: float
+        Returns
+        -------
+        The time offset of this engine
         """
 
     @delta_t.setter
     @abstractmethod
     def delta_t(self, value: float) -> None:
-        """
+        """Set the time offset of this engine.
+
         Set the value of the time offset of frame to save in seconds. If this
         isn't a multiple of the engine's timestep, the closest frame will be
         taken.
 
-        :param value: Time offset in seconds
-        :type value: float
+        Parameters
+        ----------
+        value:
+            Time offset in seconds
         """
 
     @abstractmethod
     def get_engine_str(self) -> str:
-        """
-        Get the string representation of this engine. This is what's used to
-        validate with the JSON inputs
+        """Get the string representation of this engine.
 
-        :return: String of the engine's representation
+        This string is what defines the engine name in the inputs dictionary.
+
+        Returns
+        -------
+        String of the engine's representation
         """
         pass
