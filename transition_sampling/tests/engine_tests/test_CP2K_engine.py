88c16c7a817afaf7eb34cdd1cb8fd7ea082cfd77
Isaiah Lemmon <lemmoi@cs.washington.edu>
2020-12-02 19:46:05 -0800

    tests merged

--- transition_sampling/tests/engine_tests/test_CP2K_engine.py
+++ transition_sampling/tests/engine_tests/test_CP2K_engine.py
@@ -6,10 +6,12 @@ from unittest import TestCase
 import numpy as np
 
 from engines import CP2KEngine
+from engines.CP2K_engine import write_cp2k_input
 
 ENG_STR = "cp2k"
 cur_dir = os.path.dirname(__file__)
 TEST_INPUT = os.path.join(cur_dir, "test_data/test_cp2k.inp")
+TEST_CMD = "test cmd"
 
 
 # Parsing the input into memory for every test makes these pretty slow ~1s each,
@@ -25,7 +27,8 @@ class CP2KEngineTestCase(TestCase):
 
     def setUp(self) -> None:
         self.engine = CP2KEngine({"engine": ENG_STR,
-                                  "cp2k_inputs": TEST_INPUT})
+                                  "cp2k_inputs": TEST_INPUT,
+                                  "cmd": TEST_CMD})
         self.assertEqual(len(self.engine.atoms), 2)
 
 
@@ -37,12 +40,29 @@ class TestCP2KEngineValidation(TestCase):
         # Invalid name will raise exception
         with self.assertRaises(ValueError, msg="Empty engine name should fail"):
             e = CP2KEngine({"engine": "",
+                            "cp2k_inputs": TEST_INPUT,
+                            "cmd": TEST_CMD})
+
+        # Valid engine name should pass
+        e = CP2KEngine({"engine": ENG_STR,
+                        "cp2k_inputs": TEST_INPUT,
+                        "cmd": TEST_CMD})
+        self.assertIsNotNone(e, f"{ENG_STR} should be valid")
+
+    def test_cmd(self):
+        """
+        Command should be a string
+        """
+        # Invalid name will raise exception
+        with self.assertRaises(ValueError, msg="Empty engine name should fail"):
+            e = CP2KEngine({"engine": ENG_STR,
                             "cp2k_inputs": TEST_INPUT})
 
         # Valid engine name should pass
         e = CP2KEngine({"engine": ENG_STR,
-                        "cp2k_inputs": TEST_INPUT})
-        self.assertIsNotNone(e, f"{ENG_STR} should be valid")
+                        "cp2k_inputs": TEST_INPUT,
+                        "cmd": TEST_CMD})
+        self.assertIsNotNone(e, f"{TEST_CMD} should be valid")
 
     def test_missing_input_file(self):
         """
@@ -51,13 +71,15 @@ class TestCP2KEngineValidation(TestCase):
         # Missing input field
         with self.assertRaises(ValueError,
                                msg="Missing input field should fail"):
-            e = CP2KEngine({"engine": ENG_STR})
+            e = CP2KEngine({"engine": ENG_STR,
+                            "cmd": TEST_CMD})
 
         # Input file does not exist
         with self.assertRaises(ValueError,
                                msg="Non-existent input file should fail"):
             e = CP2KEngine({"engine": ENG_STR,
-                            "cp2k_inputs": "non_existent_file"})
+                            "cp2k_inputs": "non_existent_file",
+                            "cmd": TEST_CMD})
 
     def test_invalid_input_file(self):
         """
@@ -78,14 +100,16 @@ class TestCP2KEngineValidation(TestCase):
             with self.assertRaises(ValueError,
                                    msg="Invalid CP2K input should fail"):
                 e = CP2KEngine({"engine": ENG_STR,
-                                "cp2k_inputs": temp_file.name})
+                                "cp2k_inputs": temp_file.name,
+                                "cmd": TEST_CMD})
 
     def test_valid_input_file(self):
         """
         Provided input file should be valid
         """
         self.assertIsNotNone(CP2KEngine({"engine": ENG_STR,
-                                         "cp2k_inputs": TEST_INPUT}),
+                                         "cp2k_inputs": TEST_INPUT,
+                                         "cmd": TEST_CMD}),
                              msg="Test input should be valid")
 
 
@@ -137,12 +161,37 @@ class TestCP2KEnginePositions(CP2KEngineTestCase):
 
         self.engine.set_positions(pos)
 
-        # Internal Representation of stored positions for CP2K
-        stored_pos = \
-            self.engine.cp2k_inputs["+force_eval"][0]["+subsys"]["+coord"]["*"]
+        self._compare_positions(pos, self.engine)
+
+    def test_set_positions_and_write(self):
+        """
+        Assign positions, write to a file, load into a new engine, and see if
+        they match
+        """
+        pos = np.array([[1.0021, 123.123, 6.23123],
+                        [8.12, 6.12381, 0.1232]])
+
+        self.engine.set_positions(pos)
+        with tempfile.NamedTemporaryFile() as temp_file:
+            write_cp2k_input(self.engine.cp2k_inputs, temp_file.name)
+
+            new_engine = CP2KEngine({"engine": ENG_STR,
+                                     "cp2k_inputs": temp_file.name,
+                                     "cmd": TEST_CMD})
+
+            self._compare_positions(pos, new_engine)
+
+    def _compare_positions(self, expected, engine):
+        """
+        Compare expected positions to those actually stored by an engine
+        :param expected: Array of expected positions
+        :param engine: Engine to compare to
+        """
+        actual = \
+            engine.cp2k_inputs["+force_eval"][0]["+subsys"]["+coord"]["*"]
 
         # Iterate over each stored and assigned position to compare
-        for s, p in zip(stored_pos, pos):
+        for s, p in zip(actual, expected):
             # Convert string representation to list of floats
             split_list = [float(num) for num in s[3:].split()]
             # Convert numpy array to list for assertListEquals
@@ -153,7 +202,7 @@ class TestCP2KEnginePositions(CP2KEngineTestCase):
 class TestCP2KEngineVelocities(CP2KEngineTestCase):
     def test_set_velocities_wrong_num_atoms(self):
         """
-        Test there must be exactly one velocityector for each atom
+        Test there must be exactly one velocity vector for each atom
         """
         vel = np.array([[1.0021, 123.123, 1.2012]])
 
@@ -172,7 +221,7 @@ class TestCP2KEngineVelocities(CP2KEngineTestCase):
                                msg="There should be an x,y,z for each atom"):
             self.engine.set_velocities(vel)
 
-    def test_set_positions_valid(self):
+    def test_set_velocities_valid(self):
         """
         Assign valid velocities and check the internal representation of them
         """
@@ -180,14 +229,40 @@ class TestCP2KEngineVelocities(CP2KEngineTestCase):
                         [8.12, 6.12381, 0.1232]])
 
         self.engine.set_velocities(vel)
+        self._compare_velocities(vel, self.engine)
 
+    def test_set_velocities_and_write(self):
+        """
+        Assign velocities, write to a file, load into a new engine, and see if
+        they match
+        """
+        vel = np.array([[1.0021, 123.123, 6.23123],
+                        [8.12, 6.12381, 0.1232]])
+
+        self.engine.set_velocities(vel)
+
+        with tempfile.NamedTemporaryFile() as temp_file:
+            write_cp2k_input(self.engine.cp2k_inputs, temp_file.name)
+
+            new_engine = CP2KEngine({"engine": ENG_STR,
+                                     "cp2k_inputs": temp_file.name,
+                                     "cmd": TEST_CMD})
+
+            self._compare_velocities(vel, new_engine)
+
+    def _compare_velocities(self, expected, engine):
+        """
+        Compare the expected values of a velocity to those actually stored by
+        an engine
+        :param expected: array of expected velocities
+        :param engine: engine to check if velocities match
+        """
         # Internal Representation of stored positions for CP2K
-        stored_vel = \
-            self.engine.cp2k_inputs["+force_eval"][0]["+subsys"]["+velocity"][
-                "*"]
+        actual = \
+            engine.cp2k_inputs["+force_eval"][0]["+subsys"]["+velocity"]["*"]
 
         # Iterate over each stored and assigned position to compare
-        for s, v in zip(stored_vel, vel):
+        for s, v in zip(actual, expected):
             # Convert numpy array to list for assertListEquals
             v = v.tolist()
             self.assertListEqual(v, s, "Velocities were not equal")
