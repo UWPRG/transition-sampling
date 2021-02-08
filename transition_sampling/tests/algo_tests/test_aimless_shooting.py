a244ce430936612744c308cb4e729be499d3234c
Luke Gibson <ldgibson@uw.edu>
2021-02-08 15:23:08 -0800

    adding unit test for velocity generation

--- transition_sampling/tests/algo_tests/test_aimless_shooting.py
+++ transition_sampling/tests/algo_tests/test_aimless_shooting.py
@@ -2,6 +2,7 @@ import unittest
 
 from transition_sampling.engines import ShootingResult
 from transition_sampling.algo import AimlessShooting
+from transition_sampling.algo.aimless_shooting import generate_velocities
 import numpy as np
 
 
@@ -31,6 +32,45 @@ class NextPositionTest(unittest.TestCase):
 
             picked = aimless.pick_starting(test_result)
             self.assertEqual(correct_choice[i], picked[0, 0])
+
+
+class VelocityGenerationTest(unittest.TestCase):
+    """Test that velocity generation works"""
+    def test_generate_velocities(self):
+
+        # Set the seed for reproducible results.
+        np.random.seed(1)
+
+        test_atoms = ['Ar'] * 1000
+        test_temp1 = 300  # K
+        test_temp2 = 1000  # K
+
+        test_vel1 = generate_velocities(test_atoms, test_temp1)
+        test_vel2 = generate_velocities(test_atoms, test_temp2)
+
+        # Assert that a numpy array is returned.
+        self.assertTrue(isinstance(test_vel1, np.ndarray))
+
+        # Test that the shape of the velocities are correct.
+        self.assertEqual(test_vel1.shape, (len(test_atoms), 3))
+
+        # Histogram each velocity distribution and assert that the peak 
+        # for T = 300 K is higher and occurs at a lower temperature
+        # than T = 1000 K.
+        test_vel_mag1 = np.linalg.norm(test_vel1, axis=1)
+        test_vel_mag2 = np.linalg.norm(test_vel2, axis=1)
+
+        counts1, _ = np.histogram(test_vel_mag1, bins=20, range=(1e-5, 1e-3))
+        counts2, _ = np.histogram(test_vel_mag2, bins=20, range=(1e-5, 1e-3))
+
+        max1 = np.max(counts1)
+        max2 = np.max(counts2)
+
+        max_loc1 = np.argmax(counts1)
+        max_loc2 = np.argmax(counts2)
+
+        self.assertTrue(max1 > max2)
+        self.assertTrue(max_loc1 < max_loc2)
 
 
 if __name__ == '__main__':
