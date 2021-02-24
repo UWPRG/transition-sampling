6dfc5be0299e1db6bfd61b8e0e36311a1a12f9f7
Isaiah Lemmon <lemmoi@uw.edu>
2021-02-23 21:32:46 -0800

    Test for invalid plumed file

--- transition_sampling/tests/integration_tests/colvar_tests/test_plumed_driver_integration.py
+++ transition_sampling/tests/integration_tests/colvar_tests/test_plumed_driver_integration.py
@@ -1,15 +1,13 @@
 import os
 import tempfile
 from unittest import TestCase
-import random
-
-import numpy as np
-import pandas as pd
+import subprocess
 
 from transition_sampling.colvar import PlumedDriver
 
 CUR_DIR = os.path.dirname(__file__)
 DEFAULT_PLUMED_DAT = os.path.join(CUR_DIR, "../shared_test_data/plumed.dat")
+FAILING_PLUMED_DAT = os.path.join(CUR_DIR, "test_data/failing_plumed.dat")
 
 # Paths of starting configs and stored results
 INPUT_XYZ = os.path.join(CUR_DIR, "test_data/input.xyz")
@@ -29,3 +27,13 @@ class TestPlumedDriverIntegration(TestCase):
             driver = PlumedDriver(PLUMED_BIN)
 
             driver.run(DEFAULT_PLUMED_DAT, INPUT_XYZ, INPUT_CSV, results_colvar)
+
+    def test_invalid_plumed_fails(self):
+        with tempfile.TemporaryDirectory() as directory:
+            results_colvar = f"/tmp/COLVAR"
+
+            driver = PlumedDriver(PLUMED_BIN)
+
+            with self.assertRaises(subprocess.CalledProcessError,
+                                   msg="Invalid plumed file should have raised an exception"):
+                driver.run(FAILING_PLUMED_DAT, INPUT_XYZ, INPUT_CSV, results_colvar)
