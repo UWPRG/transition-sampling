4fd28af015d965a69fd66f8721b78c121197b351
Isaiah Lemmon <lemmoi@uw.edu>
2021-02-24 17:11:08 -0800

    Compare to expected file

--- transition_sampling/tests/integration_tests/colvar_tests/test_plumed_driver_integration.py
+++ transition_sampling/tests/integration_tests/colvar_tests/test_plumed_driver_integration.py
@@ -12,6 +12,7 @@ FAILING_PLUMED_DAT = os.path.join(CUR_DIR, "test_data/failing_plumed.dat")
 # Paths of starting configs and stored results
 INPUT_XYZ = os.path.join(CUR_DIR, "test_data/input.xyz")
 INPUT_CSV = os.path.join(CUR_DIR, "test_data/input.csv")
+EXPECTED_DEFAULT_CV = os.path.join(CUR_DIR, "test_data/expected_default_COLVAR")
 
 PLUMED_BIN = "plumed"
 
@@ -22,18 +23,24 @@ class TestPlumedDriverIntegration(TestCase):
     def test_integration(self):
         # Create directory for algo results and engine working space
         with tempfile.TemporaryDirectory() as directory:
-            results_colvar = f"/tmp/COLVAR"
+            results_colvar = f"{directory}/COLVAR"
 
             driver = PlumedDriver(PLUMED_BIN)
 
             driver.run(DEFAULT_PLUMED_DAT, INPUT_XYZ, INPUT_CSV, results_colvar)
 
+            # Compare files as a list so there is feedback on differences
+            self.assertListEqual([row for row in open(EXPECTED_DEFAULT_CV)],
+                                 [row for row in open(results_colvar)],
+                                 msg="Files are expected to be equal")
+
     def test_invalid_plumed_fails(self):
         with tempfile.TemporaryDirectory() as directory:
-            results_colvar = f"/tmp/COLVAR"
+            results_colvar = f"{directory}/COLVAR"
 
             driver = PlumedDriver(PLUMED_BIN)
 
             with self.assertRaises(subprocess.CalledProcessError,
                                    msg="Invalid plumed file should have raised an exception"):
-                driver.run(FAILING_PLUMED_DAT, INPUT_XYZ, INPUT_CSV, results_colvar)
+                driver.run(FAILING_PLUMED_DAT, INPUT_XYZ, INPUT_CSV,
+                           results_colvar)
